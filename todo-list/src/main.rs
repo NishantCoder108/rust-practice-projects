1507454baef11c36206c0f3af5098cc52856dad0
Nishant <69218488+NishantCoder108@users.noreply.github.com>
2025-04-28 20:51:52 +0530

    created a promised world

--- todo-list/src/main.rs
+++ todo-list/src/main.rs
@@ -1,3 +1,35 @@
+use std::io::{self, Read};
+
 fn main() {
-    println!("Hello, world!");
+    promise_world();
+}
+
+fn promise_world() {
+    loop {
+        println!("\n\n\n📝 WELCOME TO YOUR PROMISE WORLD!");
+        println!("=================================");
+        println!("\nA place where you promise yourself to complete what matters.\n");
+
+        println!("1. ➕ Add Your Promise");
+        println!("2. 📋 View Your Promises");
+        println!("3. ✅ Complete Your Promise");
+        println!("4. 🔄 Reaffirm Your Promise");
+        println!("5. 🚪 Exit\n");
+
+        //Here give choices:
+        let mut choice = String::new();
+        io::stdin()
+            .read_line(&mut choice)
+            .expect("Failed to read line");
+
+        // let choice: u8 = choice.trim().parse().expect("Please type number!");
+        let choice: u8 = if let Ok(num) = choice.trim().parse() {
+            num
+        } else {
+            println!("Invalid number! , Please try again.");
+            continue;
+        };
+
+        println!("Choices : {:?}", choice);
+    }
 }
